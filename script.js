d3f4c0537f01301d8788244db109c9b56cd6a186
Ivan Roderick Salcedo <nj.ivan.salcedo@gmail.com>
2025-07-09 20:52:30 -0400

    Add operate and updateDisplay functions

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+let firstOperand;
+let operator;
+let secondOperand;
+
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('#results-display');
+
+function updateDisplay(content) {
+    display.value = content;
+}
+
+function operate(operator, firstOperand, secondOperand) {
+    switch (operator) {
+        case '+':
+            return add(firstOperand, secondOperand);
+        case '-':
+            return subtract(firstOperand, secondOperand);
+        case '×':
+            return multiply(firstOperand, secondOperand);
+        case '÷':
+            return divide(firstOperand, secondOperand);
+        default:
+            return 'Error';
+    }
+}
+
 function add(a, b) {
     return a + b;
 }
